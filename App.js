8eaf391aa3389862ce64b274fea8dbb59999d155
irishsiberian <irishsiberian@gmail.com>
2021-03-26 15:21:05 +0700

    add webview

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={{height: 600, width: 400,overflow:'hidden',marginTop: 15 }}>
+
+        <WebView
+            source={{uri: 'https://apptest.mathkang.ru'}}
+            scalesPageToFit={true}
+            domStorageEnabled={true}
+            javaScriptEnabled={true}
+            startInLoadingState={true}
+
+        />
+          <Text>Open up App.js to start working on your app!</Text>
+      </View>
   );
 }
 
